fde674fd3b6d45c2c0bed4d0139e1c6830735128
Jens Eickelmann <jens.eickelmann@isc-konstanz.de>
2023-11-23 12:49:48 +0100

    Added docstrings

--- scisys/io/pdf.py
+++ scisys/io/pdf.py
@@ -73,6 +73,16 @@ class PDF:
               + self.general_dict["project"])
 
     def draw_layout(self, logo_dir: str, logo: str, headline: str = "", explanation: str = ""):
+        """
+        Draws a layout with logo, Header and Footer.
+
+        :param logo_dir: Path to the logo file directory.
+        :param logo: Name of the logo file.
+        :param headline: Headline, written in the Header
+        :param explanation: Sub-Headline, explaining the page content.
+        :return:
+        """
+
         print("[PDFCreator] Creating Page " + headline)
         # Header
         self.canvas.drawImage(os.path.join(logo_dir, logo),
@@ -97,6 +107,18 @@ class PDF:
         self.canvas.drawString(self.left_margin * mm, 7 * mm, str(self.general_dict["date"]))
 
     def insert_image(self, image_dir, image_file: str, x_cord: int, y_cord: int, width: int = None, height: int = None):
+        """
+        Inserts an image in the current PDF Page.
+
+        :param image_dir: Path to the image file directory.
+        :param image_file: Name of the image file.
+        :param x_cord: x-coordinate the image will be inserted at in the current PDF Page
+        :param y_cord: y-coordiante the image will be inserted at in the current PDF Page
+        :param width: Maximum width of the image. Ratio will be kept.
+        :param height: Maximum height of the image. Ratio will be kept.
+        :return:
+        """
+
         image = Image.open(os.path.join(self.project_dir, image_dir, image_file))
 
         img_height = image.height
@@ -119,6 +141,18 @@ class PDF:
 
     def insert_textlines(self, text: str or list, x_cord: int, y_cord: int, textcolor: str = "black",
                          font: str = "", fontsize: int = 0):
+        """
+        Inserts textlines, either from a string or a list.
+
+        :param text: Text string or list. Every String in a list is a new line.
+        :param x_cord: x-coordinate the image will be inserted at in the current PDF Page
+        :param y_cord: y-coordinate the image will be inserted at in the current PDF Page
+        :param textcolor: Color of the text.
+        :param font: Font of the text. If none is passed, font of the PDF will be taken.
+        :param fontsize: Fontsize of the text. If none is passed, content fontsize of the PDF will be taken.
+        :return:
+        """
+
         if fontsize == 0:
             fontsize = self.fontsize_content
         if font == "":
@@ -131,6 +165,16 @@ class PDF:
         self.canvas.drawText(text_configuration)
 
     def insert_textfile(self, text_dir: str, text_file: str, x_cord: int, y_cord: int):
+        """
+        Inserts text from a textfile (.txt) to the current PDF Page. Text can be formatted in XML.
+
+        :param text_dir: Path to the text file directory.
+        :param text_file: Name of the text file.
+        :param x_cord: x-coordinate the text will be inserted at in the current PDF Page
+        :param y_cord: y-coordinate the text will be inserted at in the current PDF Page
+        :return:
+        """
+
         if text_file.endswith(".txt"):
             with open(os.path.join(self.project_dir, text_dir, text_file)) as text:
                 text = Paragraph(text.read())
@@ -144,6 +188,25 @@ class PDF:
                          fillcolor: colors = colors.white,
                          borderwidth: int = 0,
                          bordercolor: colors = colors.white):
+        """
+        Inserts a Textfield to the current PDF Page. A textfield can be edited within the PDF Viewer.
+
+        :param x_cord: x-coordinate the textfield will be inserted at in the current PDF Page
+        :param y_cord: y-coordinate the textfield will be inserted at in the current PDF Page
+        :param width: Width of the textfield in mm
+        :param height: Height of the textfield in mm
+        :param max_length: Max length of the text that can be written in the PDF Viewer
+        :param text: Pre-Text that can be edited in the PDF Viewer
+        :param tooltip: Tooltip to be shwon in the PDF viewer
+        :param font: Font of the text. If none is passed, font of the PDF will be taken.
+        :param fontsize: Fontsize of the text. If none is passed, content fontsize of the PDF will be taken.
+        :param textcolor: Color of the text. If none is passed, it's "black"
+        :param fillcolor: Background color of the textfield. If none is passed, it's "white"
+        :param borderwidth: Width of the textfield border. If none is passed, it's 0.
+        :param bordercolor: Color of the textfield border. If none is passed, it's "white".
+        :return:
+        """
+
         if fontsize == 0:
             fontsize = self.fontsize_content
         if font == "":
@@ -162,6 +225,16 @@ class PDF:
                                        tooltip=tooltip)
 
     def insert_section_header(self, text: str, y_cord: int, textcolor: str = "black"):
+        """
+        Insert a section header to the current PDF Page. Consists of a centered text,
+        with lines to the left and right.
+
+        :param text: Text / Name of the section header.
+        :param y_cord: y-coordinate the section header will be inserted at in the current PDF Page
+        :param textcolor: Color of the text. If none is passed, it's "black"
+        :return:
+        """
+
         self.canvas.setFont(self.font_bold, 14)
         self.canvas.setFillColor(textcolor)
         self.canvas.drawCentredString(105 * mm, y_cord * mm, text)
@@ -172,12 +245,34 @@ class PDF:
                          (y_cord + 1) * mm)
 
     def insert_headline(self, text: str, y_cord: int, textcolor: str = "black"):
+        """
+        Inserts a bolded headline to the current PDF Page.
+
+        :param text: Text / Name of the current Headline
+        :param y_cord: y-coordinate the Headline will be inserted at in the current PDF Page
+        :param textcolor: Color of the Headline. If none is passed, it's "black"
+        :return:
+        """
+
         self.canvas.setFont(self.font_bold, self.fontsize_content + 2)
         self.canvas.setFillColor(textcolor)
         self.canvas.drawString(self.left_margin * mm, y_cord * mm, text)
 
     def insert_table(self, table_data: pd.DataFrame, x_cord: int, y_cord: int, row_colors: list = None,
                      textcolor: str = "black", linecolor: str = "grey", removeindex: bool = True):
+        """
+        Inserts a table to the current PDF Page.
+
+        :param table_data: Table as Pandas Dataframe.
+        :param x_cord: x-coordinate the Table will be inserted at in the current PDF Page
+        :param y_cord: y-coordinate the Table will be inserted at in the current PDF Page
+        :param row_colors: Color of the Table's rows as a list. Every list item is one row.
+        :param textcolor: Color of the text in the table, not specified by "row_colors". If none is passed, it's "black"
+        :param linecolor: Color of the Table's lines
+        :param removeindex: Removes the Pandas Dataframe Index if True. If False,
+        index will be written bolded at the top of the table
+        :return:
+        """
 
         if removeindex:
             table_data.index.rename("", inplace=True)
@@ -203,7 +298,13 @@ class PDF:
         table.drawOn(self.canvas, x_cord * mm, y_cord * mm)
 
     def create_cover_sheet(self):
-        # Create First Page
+        """
+        Creates a pre-defined Cover Sheet with Title, Project, Confidentiality,
+        Author and Date as defined in the config file
+
+        :return:
+        """
+
         self.canvas.drawImage(os.path.join("layout", self.pictures_dict["logo_cover"]), 10 * mm, 260 * mm,
                               width=(293 / 8) * mm, height=(185 / 8) * mm)
 
@@ -225,6 +326,13 @@ class PDF:
         self.canvas.showPage()
 
     def savepdf(self, openpdf: bool = False):
+        """
+        Saves the current PDF.
+
+        :param openpdf: Opens the PDF in Standard PDF Viewer, if True.
+        :return:
+        """
+
         print("[PDFCreator] Saving PDF to " + str(self.pdf_path))
         self.canvas.save()
 
@@ -232,6 +340,13 @@ class PDF:
             os.startfile(self.pdf_path)
 
     def convert_pdf_to_docx(self, opendocx: bool = False):
+        """
+        Converts the saved PDF to docx with pdf2docx.
+
+        :param opendocx: Opens the docx in Word, if True.
+        :return:
+        """
+
         from pdf2docx import parse
 
         parse(self.pdf_path,
